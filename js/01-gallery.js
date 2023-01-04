c4712c4576ad8eb0032be9a4d7e9d19f04ecb492
Anastasiia2827 <nasty.bond2016@gmail.com>
2023-01-04 21:38:39 +0200

    1task

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,41 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+const galleryContainer = document.querySelector(".gallery");
+
+const markup = galleryItems.map(({ preview, original, description }) =>
+    `<div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+        <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+        />
+        </a>
+        </div>`).join("");
+
+galleryContainer.insertAdjacentHTML('beforeend', markup);
+// console.log(markup); 
+
+galleryContainer.addEventListener('click', onclick);
+
+function onclick(evt) {
+    evt.preventDefault();
+    modalImg(evt); 
+}
+
+function modalImg(evt) {
+    const instance =  basicLightbox.create(
+        `<img src="${evt.target.dataset.source}" width="800" height="600">`
+    ).show()   
+
+}
+
+// document.querySelector('div.gallery').onclick = () => {
+
+// 	basicLightbox.create(`
+// 		<img width="1400" height="900" scr="">
+// 	`).show()
